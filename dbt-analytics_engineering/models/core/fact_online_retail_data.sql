167bb28549f55c18fc2823f792cbb9e5de789495
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-04-17 12:19:51 +0000

    fact_table and dbt test

--- dbt-analytics_engineering/models/core/fact_online_retail_data.sql
+++ dbt-analytics_engineering/models/core/fact_online_retail_data.sql
@@ -1,4 +1,12 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "invoice_datetime",
+      "data_type": "timestamp",
+      "granularity": "day"
+    },
+    cluster_by=["customer_id"]
+) }}
 
-select *
+SELECT *
 from {{ ref('stg_online_retail_data') }}
